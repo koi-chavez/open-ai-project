d7a63948afda714691677e31e5bbe585a38bb111
koi-chavez <kdc0820@westminstercollege.edu>
2022-05-10 12:44:28 -0600

    Logic/ UI Updates

--- src/App.js
+++ src/App.js
@@ -1,89 +1,98 @@
 import { useState } from 'react';
 import './App.css';
-
-// TO DO: 
-// Create button to choose a prompt for the user
-// Clean up UI
-
-
+import randomQuestion from './random';
+ 
 function App() {
-  const [promptValue, setPromptValue] = useState('');
-  const [responseArray, setResponseArray] = useState([]);
-  const [promptArray, setPromptArray] = useState([]);
-
-  let data = {
-    prompt: promptValue,
-    temperature: 0.5,
-    max_tokens: 64,
-    top_p: 1.0,
-    frequency_penalty: 0.0,
-    presence_penalty: 0.0,
-   };
-
-   async function fetchResponse() {
-    const response = await fetch(
-        `https://api.openai.com/v1/engines/text-curie-001/completions`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${process.env.REACT_APP_OPENAI_SECRET}`,
-          },
-          body: JSON.stringify(data),
-         });
-
-    let aiResponse;
-    try {
-      aiResponse = await response.json();
-    } catch (err) {
-        console.log('Response Error', err);
-    }
-    setPromptArray(oldArray => [...oldArray, promptValue]);
-    setResponseArray(oldArray => [...oldArray, aiResponse?.choices?.[0]?.text]);
-  }
-
-  async function handleSubmit(event) {
-    event.preventDefault();
-    await fetchResponse();
-  }
-
-  const ResponseUI = () => {
-    return (
-      <ul>
-        {
-      promptArray.slice(0).reverse().map((item, index) => {
-        return (
-          <li key={index}>
-            <div>
-              <strong>Prompt: </strong>
-              {item}
-            </div>
-            <div>
-              <strong>Response: </strong>
-              {responseArray[promptArray.indexOf(item)]}
-            </div>
-          </li>
-        );
-      }
-      )
-    }
-      </ul>
-    )
-  }
-
-  return (
-    <div className="App">
-      <h1> Fun with AI </h1>
-      Please enter a prompt:
-      <form id="prompt-form" className="prompt">
-        <input type="text" id="text-input" className="textBox" onChange={ (event) => setPromptValue(event.target.value) }/>
-        <br />
-        <input type="submit" value="Submit" onClick={handleSubmit} className="submitButton" />
-      </form>
-      <h1> Responses </h1>
-      <ResponseUI />
-    </div>
-  );
+ const [promptValue, setPromptValue] = useState('');
+ const [responseArray, setResponseArray] = useState([]);
+ const [promptArray, setPromptArray] = useState([]);
+ 
+ let data = {
+   prompt: promptValue,
+   temperature: 0.5,
+   max_tokens: 64,
+   top_p: 1.0,
+   frequency_penalty: 0.0,
+   presence_penalty: 0.0,
+  };
+ 
+  async function fetchResponse() {
+   const response = await fetch(
+       `https://api.openai.com/v1/engines/text-curie-001/completions`,
+       {
+         method: "POST",
+         headers: {
+           "Content-Type": "application/json",
+           Authorization: `Bearer ${process.env.REACT_APP_OPENAI_SECRET}`,
+         },
+         body: JSON.stringify(data),
+        });
+ 
+   let aiResponse;
+   try {
+     aiResponse = await response.json();
+   } catch (err) {
+       console.log('Response Error', err);
+   }
+   setPromptArray(oldArray => [...oldArray, promptValue]);
+   setResponseArray(oldArray => [...oldArray, aiResponse?.choices?.[0]?.text]);
+ }
+ 
+ async function handleSubmit(event) {
+   event.preventDefault();
+   await fetchResponse();
+ }
+ 
+ async function choosePrompt(event) {
+   event.preventDefault();
+   let randomNum = Math.floor(Math.random() * 50);
+   let randomPrompt = randomQuestion.results[randomNum].question;
+   setPromptValue(randomPrompt);
+   document.querySelector('#text-input').value = `${randomPrompt}`;
+ }
+ 
+ 
+ const ResponseUI = () => {
+   return (
+     <ul>
+       {
+     promptArray.slice(0).reverse().map((item, index) => {
+       return (
+         <li key={index}>
+           <div className='listItem'>
+             <strong>Prompt: </strong>
+             {item}
+           </div>
+           <div className='listItem'>
+             <strong>Response: </strong>
+             {responseArray[promptArray.indexOf(item)]}
+           </div>
+         </li>
+       );
+     }
+     )
+   }
+     </ul>
+   )
+ }
+ 
+ return (
+   <div className="App">
+     <h1> Fun with AI </h1>
+     Please enter a prompt:
+     <form id="prompt-form" className="prompt">
+       <input type="text" id="text-input" className="textBox" onChange={ (event) => setPromptValue(event.target.value) }/>
+       <br />
+       <input type="submit" value="Submit" onClick={handleSubmit} className="button" />
+       <button className='button' onClick={choosePrompt}>Pick a Prompt for me</button>
+     </form>
+    
+     <h1> Responses </h1>
+     <ResponseUI />
+   </div>
+ );
 }
-
+ 
 export default App;
+ 
+
---
+++ src/random.js
@@ -0,0 +1,580 @@
+const randomQuestion = {
+    "results": [
+        {
+            "category": "Vehicles",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "Where are the cars of the brand 'Ferrari' manufactured?",
+            "correct_answer": "Italy",
+            "incorrect_answers": [
+            "Romania",
+            "Germany",
+            "Russia"
+        ]
+        },
+        {
+            "category": "Entertainment: Books",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "George Orwell wrote this book, which is often considered a statement on government oversight.",
+            "correct_answer": "1984",
+            "incorrect_answers": [
+            "The Old Man and the Sea",
+            "Catcher and the Rye",
+            "To Kill a Mockingbird"
+            ]
+        },
+        {
+            "category": "Entertainment: Film",
+            "type": "multiple",
+            "difficulty": "hard",
+            "question": "Which was the first of Alfred Hitchcock's movies to be filmed in colour?",
+            "correct_answer": "Rope",
+            "incorrect_answers": [
+            "Psycho",
+            "Vertigo",
+            "Rebecca"
+            ]
+        },
+        {
+            "category": "General Knowledge",
+            "type": "multiple",
+            "difficulty": "hard",
+            "question": "What type of dog is 'Handsome Dan', the mascot of Yale University?",
+            "correct_answer": "Bulldog",
+            "incorrect_answers": [
+            "Yorkshire Terrier",
+            "Boxer",
+            "Pug"
+            ]
+        },
+        {
+            "category": "Entertainment: Video Games",
+            "type": "multiple",
+            "difficulty": "hard",
+            "question": "How many people can you recruit in the game Suikoden in a single playthrough?",
+            "correct_answer": "107",
+            "incorrect_answers": [
+            "108",
+            "93",
+            "96"
+            ]
+        },
+        {
+            "category": "History",
+            "type": "boolean",
+            "difficulty": "medium",
+            "question": "The two atomic bombs dropped on Japan by the United States in August 1945 were named 'Little Man' and 'Fat Boy'.",
+            "correct_answer": "False",
+            "incorrect_answers": [
+            "True"
+            ]
+        },
+        {
+            "category": "General Knowledge",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "Gumbo is a stew that originated in Louisiana.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+            "False"
+            ]
+        },
+        {
+            "category": "Geography",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "San Marino is the only country completely surrounded by another country.",
+            "correct_answer": "False",
+            "incorrect_answers": [
+            "True"
+            ]
+        },
+        {
+            "category": "History",
+            "type": "boolean",
+            "difficulty": "medium",
+            "question": "In World War II, Hawker Typhoons served in the Pacific theater.",
+            "correct_answer": "False",
+            "incorrect_answers": [
+            "True"
+            ]
+        },
+        {
+            "category": "Entertainment: Film",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "Who played Batman in the 1997 film 'Batman and Robin'?",
+            "correct_answer": "George Clooney",
+            "incorrect_answers": [
+            "Michael Keaton",
+            "Val Kilmer",
+            "Christian Bale"
+            ]
+        },
+        {
+            "category": "Science & Nature",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "What is the primary addictive substance found in tobacco?",
+            "correct_answer": "Nicotine",
+            "incorrect_answers": [
+            "Cathinone",
+            "Ephedrine",
+            "Glaucine"
+            ]
+        },
+        {
+            "category": "Animals",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "What is the scientific name for the 'Polar Bear'?",
+            "correct_answer": "Ursus Maritimus",
+            "incorrect_answers": [
+            "Polar Bear",
+            "Ursus Spelaeus",
+            "Ursus Arctos"
+            ]
+        },
+        {
+            "category": "History",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "Adolf Hitler was a german soldier in World War I.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+            "False"
+            ]
+        },
+        {
+            "category": "Entertainment: Japanese Anime & Manga",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "Gosho Aoyama Made This Series: (Detective Conan / Case Closed!)",
+            "correct_answer": "True",
+            "incorrect_answers": [
+            "False"
+            ]
+        },
+        {
+            "category": "Entertainment: Video Games",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "The Touhou Project series of games is often associated with which genre?",
+            "correct_answer": "Shoot 'em up",
+            "incorrect_answers": [
+            "Strategy",
+            "FPS",
+            "Casual"
+            ]
+        },
+        {
+            "category": "Entertainment: Video Games",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "In 'Fallout 4' which faction is not present in the game?",
+            "correct_answer": "The Enclave",
+            "incorrect_answers": [
+            "The Minutemen",
+            "The Brotherhood of Steel",
+            "The Institute"
+            ]
+        },
+        {
+            "category": "Entertainment: Video Games",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "What type of genre is the controversial 2015 game 'Hatred'?",
+            "correct_answer": "Shoot 'Em Up",
+            "incorrect_answers": [
+            "Point &amp; Click",
+            "MMORPG",
+            "Simulation"
+            ]
+        },
+        {
+            "category": "History",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "Against which country did the Dutch Republic fight the Eighty Years' War?",
+            "correct_answer": "Spain",
+            "incorrect_answers": [
+            "France",
+            "England",
+            "Portugal"
+            ]
+        },
+        {
+            "category": "Science & Nature",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "What does the yellow diamond on the NFPA 704 fire diamond represent?",
+            "correct_answer": "Reactivity",
+            "incorrect_answers": [
+            "Health",
+            "Flammability",
+            "Radioactivity"
+            ]
+        },
+        {
+            "category": "Science & Nature",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "Who made the discovery of X-rays?",
+            "correct_answer": "Wilhelm Conrad R&ouml;ntgen",
+            "incorrect_answers": [
+            "Thomas Alva Edison",
+            "James Watt",
+            "Albert Einstein"
+            ]
+        },
+        {
+            "category": "Entertainment: Video Games",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "In Terraria, you can craft the Cell Phone pre-hardmode.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+            "False"
+            ]
+        },
+        {
+            "category": "Entertainment: Music",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "Which song in Drake's 'Views' features Future?",
+            "correct_answer": "Grammys",
+            "incorrect_answers": [
+            "Too Good",
+            "Faithful",
+            "Pop Style"
+            ]
+        },
+        {
+            "category": "Vehicles",
+            "type": "boolean",
+            "difficulty": "medium",
+            "question": "Bugatti was an Italian car manufacturer.",
+            "correct_answer": "False",
+            "incorrect_answers": [
+            "True"
+            ]
+        },
+        {
+            "category": "Sports",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "In association football, or soccer, a corner kick is when the game restarts after someone scores a goal.",
+            "correct_answer": "False",
+            "incorrect_answers": [
+            "True"
+            ]
+        },
+        {
+            "category": "History",
+            "type": "multiple",
+            "difficulty": "hard",
+            "question": "How many women joined the United States Armed Services during World War II?",
+            "correct_answer": "350,000",
+            "incorrect_answers": [
+            "225,000",
+            "100,000",
+            "500,000"
+            ]
+        },
+        {
+            "category": "Vehicles",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "When was Cadillac founded?",
+            "correct_answer": "1902",
+            "incorrect_answers": [
+            "1964",
+            "1898",
+            "1985"
+            ]
+        },
+        {
+            "category": "Entertainment: Video Games",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "Ellen McLain, the voice of GLaDOS in the Portal game series, is married to the voice actor for which Team Fortress 2 character?",
+            "correct_answer": "Sniper",
+            "incorrect_answers": [
+            "Heavy",
+            "Soldier",
+            "Scout"
+            ]
+        },
+        {
+            "category": "Entertainment: Video Games",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "In 'The Binding of Isaac', what is the name of the final boss that you fight in The Void?",
+            "correct_answer": "Delirium",
+            "incorrect_answers": [
+            "Mega Satan",
+            "Hush",
+            "The Lamb"
+            ]
+        },
+        {
+            "category": "Entertainment: Video Games",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "Which operation in 'Tom Clancy's Rainbow Six Siege' introduced the 'Skyscraper' map?",
+            "correct_answer": "Red Crow",
+            "incorrect_answers": [
+            "Velvet Shell",
+            "Skull Rain",
+            "Dust Line"
+            ]
+        },
+        {
+            "category": "History",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "Kublai Khan is the grandchild of Genghis Khan?",
+            "correct_answer": "True",
+            "incorrect_answers": [
+            "False"
+            ]
+        },
+        {
+            "category": "Entertainment: Japanese Anime & Manga",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "Satella in 'Re:Zero' is the witch of what?",
+            "correct_answer": "Envy",
+            "incorrect_answers": [
+            "Pride",
+            "Sloth",
+            "Wrath"
+            ]
+        },
+        {
+            "category": "Entertainment: Television",
+            "type": "boolean",
+            "difficulty": "medium",
+            "question": "Klingons once had a period of Democracy in their history, they referred to it as the 'Dark Times'.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+            "False"
+            ]
+        },
+        {
+            "category": "General Knowledge",
+            "type": "multiple",
+            "difficulty": "hard",
+            "question": "What was Bank of America originally established as?",
+            "correct_answer": "Bank of Italy",
+            "incorrect_answers": [
+            "Bank of Long Island",
+            "Bank of Pennsylvania",
+            "Bank of Charlotte"
+            ]
+        },
+        {
+            "category": "Entertainment: Video Games",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "'Metal Gear Solid 3: Snake Eater' was released in 2004.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+            "False"
+            ]
+        },
+        {
+            "category": "Geography",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "Where is the 'Sonoran Desert' located?",
+            "correct_answer": "North America",
+            "incorrect_answers": [
+            "South America",
+            "Asia",
+            "Africa"
+            ]
+        },
+        {
+            "category": "Entertainment: Video Games",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "What is the name of the first level in 'Call of Duty: World at War'?",
+            "correct_answer": "Semper Fi",
+            "incorrect_answers": [
+            "Ring of Steel",
+            "Vendetta",
+            "Eviction"
+            ]
+        },
+        {
+            "category": "Science: Computers",
+            "type": "multiple",
+            "difficulty": "hard",
+            "question": "Dutch computer scientist Mark Overmars is known for creating which game development engine?",
+            "correct_answer": "Game Maker",
+            "incorrect_answers": [
+            "Stencyl",
+            "Construct",
+            "Torque 2D"
+            ]
+        },
+        {
+            "category": "History",
+            "type": "multiple",
+            "difficulty": "hard",
+            "question": "Which of the following was not one of Joseph Stalin's ten blows during World War II?",
+            "correct_answer": "Vistula-Oder Offensive",
+            "incorrect_answers": [
+            "Crimean Offensive",
+            "Leningrad-Novgorod Offensive",
+            "Operation Bagration"
+            ]
+        },
+        {
+            "category": "Geography",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "How many independent countries are there within the continent of South America?",
+            "correct_answer": "12",
+            "incorrect_answers": [
+            "8",
+            "9",
+            "10"
+            ]
+        },
+        {
+            "category": "Entertainment: Video Games",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "The walls of the Goldenrod City Gym in 'Pok&eacute;mon Gold and Silver' are arranged in the shape of which Pok&eacute;mon?",
+            "correct_answer": "Clefairy",
+            "incorrect_answers": [
+            "Bulbasaur",
+            "Pikachu",
+            "Pidgey"
+            ]
+        },
+        {
+            "category": "Entertainment: Books",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "In Alice in Wonderland, what is the name of Alice's kitten?",
+            "correct_answer": "Dinah",
+            "incorrect_answers": [
+            "Oscar",
+            "Heath",
+            "Smokey"
+            ]
+        },
+        {
+            "category": "Entertainment: Video Games",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "What are Sans and Papyrus named after in 'Undertale'?",
+            "correct_answer": "Fonts",
+            "incorrect_answers": [
+            "Plants",
+            "Companies",
+            "Ancient writing paper"
+            ]
+        },
+        {
+            "category": "Entertainment: Japanese Anime & Manga",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "Which Japanese music group was formed to produce theme music for the anime 'Guilty Crown'?",
+            "correct_answer": "Egoist",
+            "incorrect_answers": [
+            "Goose house",
+            "Babymetal",
+            "Garnidelia"
+            ]
+        },
+        {
+            "category": "Entertainment: Books",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "The Harry Potter series of books, combined, are over 1,000,000 words in length.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+            "False"
+            ]
+        },
+        {
+            "category": "Science: Gadgets",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "Out of all of the NASA Space Shuttles, which 2 have been destroyed in disasters?",
+            "correct_answer": "Challenger and Columbia",
+            "incorrect_answers": [
+            "Enterprise and Atlantis",
+            "Discovery and Endeavour",
+            "None of the Above"
+            ]
+        },
+        {
+            "category": "Entertainment: Comics",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "What is the real name of the 'Master Of Magnetism' Magneto?",
+            "correct_answer": "Max Eisenhardt",
+            "incorrect_answers": [
+            "Charles Xavier",
+            "Pietro Maximoff",
+            "Johann Schmidt"
+            ]
+        },
+        {
+            "category": "Entertainment: Film",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "Which of the following films was NOT directed by Quentin Tarantino?",
+            "correct_answer": "From Dusk till Dawn",
+            "incorrect_answers": [
+            "Jackie Brown",
+            "Pulp Fiction",
+            "Reservoir Dogs"
+            ]
+        },
+        {
+            "category": "Entertainment: Television",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "Which of the following awards do Matt Stone and Trey Parker NOT have?",
+            "correct_answer": "Oscar",
+            "incorrect_answers": [
+            "Emmy",
+            "Tony",
+            "Grammy"
+            ]
+        },
+        {
+            "category": "Science & Nature",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "When was the first mammal successfully cloned?",
+            "correct_answer": "1996",
+            "incorrect_answers": [
+            "2009",
+            "1999",
+            "1985"
+            ]
+        },
+        {
+            "category": "Entertainment: Video Games",
+            "type": "boolean",
+            "difficulty": "medium",
+            "question": "In the Resident Evil series, Leon S. Kennedy is a member of STARS.",
+            "correct_answer": "False",
+            "incorrect_answers": [
+            "True"
+            ]
+        }
+    ]
+ };
+  
+ export default randomQuestion;
+ 
+ 
